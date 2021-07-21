ffcf60b119192870b4f0c54106972a5dc11a7c05
charlie-aaa <87749339+charlie-aaa@users.noreply.github.com>
2021-07-21 11:09:38 +0200

    1.0.0.4

---
+++ Assets/scripts/fpsCounter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsCounter : MonoBehaviour
+{
+    public GameObject fpsText;
+    private float _hudRefreshRate = 1f;
+ 
+    private float timer;
+ 
+    private void Update()
+    {
+        if (Time.unscaledTime > timer)
+        {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.GetComponent<UnityEngine.UI.Text>().text = fps.ToString();
+            timer = Time.unscaledTime + _hudRefreshRate;
+        }
+    }
+}
